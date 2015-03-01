cf8da8edb48a1c6d9a163bc2f85533f3fa793ce1
Jonathan <ja1984@gmail.com>
2015-03-01 11:15:01 +0100

    Fix with backgroundcolor and transparency

--- core/src/com/jatj/highway/gameworld/GameRenderer.java
+++ core/src/com/jatj/highway/gameworld/GameRenderer.java
@@ -58,6 +58,7 @@ public class GameRenderer {
         cam.setToOrtho(true, gameWidth, 136);
 
         batcher = new SpriteBatch();
+        batcher.enableBlending();
         batcher.setProjectionMatrix(cam.combined);
         shapeRenderer = new ShapeRenderer();
         shapeRenderer.setProjectionMatrix(cam.combined);
@@ -120,16 +121,13 @@ public class GameRenderer {
     }
 
     public void render(float delta, float runTime) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        float[] values = hextoRGB("#29343c");
+        Gdx.gl.glClearColor(values[0], values[1], values[2], 1.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batcher.begin();
-        batcher.disableBlending();
-
-        batcher.draw(bg, 0, 0, 300, 136);
-
         batcher.enableBlending();
-
+        batcher.draw(bg, 0, 0, 300, 136);
         drawRoadLines();
 
         if (myWorld.isRunning()) {
@@ -179,4 +177,36 @@ public class GameRenderer {
             unlockedAchievements.add("xxx");
         }
     }
+
+
+    /* re-map RGB colors so they can be used in OpenGL */
+    private float[] map(float[]rgb) {
+
+    /* RGB is from 0 to 255 */
+    /* THIS is from 0 to 1 (float) */
+
+        // 1 : 2 = x : 4 >>> 2
+
+    /*
+    *
+    * 240 : 255 = x : 1
+    *
+    * */
+
+        float[] result = new float[3];
+        result[0] = rgb[0] / 255;
+        result[1] = rgb[1] / 255;
+        result[2] = rgb[2] / 255;
+        return result;
+    }
+
+    private float[] hextoRGB(String hex) {
+
+        float[] rgbcolor = new float[3];
+        rgbcolor[0] = Integer.valueOf( hex.substring( 1, 3 ), 16 );
+        rgbcolor[1] = Integer.valueOf( hex.substring( 3, 5 ), 16 );
+        rgbcolor[2] = Integer.valueOf( hex.substring( 5, 7 ), 16 );
+        return map(rgbcolor);
+    }
+
 }
